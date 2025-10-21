5d80edea40717ebe210ce8819390394225bf5fb6
SergioCalcinaMuchica <scalcinamu@unsa.edu.pe>
2025-10-21 08:42:29 -0500

    Ejercicios 2 y 4

---
+++ ej.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+using vertex = int;
+
+struct Node {
+    vertex w;
+    Node* next;
+    Node(vertex w, Node* next = nullptr) : w(w), next(next) {}
+};
+
+struct Graph {
+    int V;
+    int A;
+    Node** adj;
+};
+
+static Node* NEWnode(vertex w, Node* next) {
+    return new Node(w, next);
+}
+
+Graph* GRAPHinit(int V) {
+    Graph* G = new Graph;
+    G->V = V;
+    G->A = 0;
+    G->adj = new Node*[V];
+    for (int v = 0; v < V; ++v)
+        G->adj[v] = nullptr;
+    return G;
+}
+
+void GRAPHinsertArc(Graph* G, vertex v, vertex w) {
+    for (Node* a = G->adj[v]; a != nullptr; a = a->next)
+        if (a->w == w) return;
+    G->adj[v] = NEWnode(w, G->adj[v]);
+    G->A++;
+}
+
+void UGRAPHinsertEdge(Graph* G, vertex v, vertex w) {
+    if (v == w) return;
+    GRAPHinsertArc(G, v, w);
+    GRAPHinsertArc(G, w, v);
+}
+
+//EJERCICIO 2
+int GRAPHoutdeg(Graph* G, vertex v){ //Funcion que calcula grado de salida
+    int gradoSalida = 0;
+    for(Node* a = G->adj[v]; a!=NULL; a=a->next)
+        gradoSalida++;
+    return gradoSalida;
+}
+
+int GRAPHindeg(Graph* G, vertex v){ //Funcion que calcula grado de entrada
+    int gradoEntrada = 0;
+    for(int i=0;i<=(G->V)-1;i++){
+        for(Node* a = G->adj[i]; a!=NULL;a=a->next)
+            if(a->w == v) gradoEntrada++;
+    }
+    return gradoEntrada;
+}
+
+//EJERCICIO 4
+void GRAPHshow(Graph* G){
+    for(int i=0;i<=(G->V)-1;i++){
+        cout<<"Vertice "<<i<<": "<<endl;
+        for(Node* a = G->adj[i]; a!=NULL;a=a->next)
+            cout<<a->w<<" ";
+        cout<<endl;
+    }
+}
+
+void GRAPHfree(Graph* G) {
+    for (int v = 0; v < G->V; ++v) {
+        Node* a = G->adj[v];
+        while (a != nullptr) {
+            Node* temp = a;
+            a = a->next;
+            delete temp;
+        }
+    }
+    delete[] G->adj;
+    delete G;
+}
+
+int main() {
+    Graph* G = GRAPHinit(4);
+
+    UGRAPHinsertEdge(G, 0, 1);
+    UGRAPHinsertEdge(G, 0, 2);
+    UGRAPHinsertEdge(G, 1, 2);
+    UGRAPHinsertEdge(G, 2, 3);
+
+    cout << "grafo:\n";
+    GRAPHshow(G);
+    cout<<"---"<<endl;
+    cout<<"grado salida de vertice 0:"<<GRAPHoutdeg(G,0)<<endl;
+    cout<<"grado entrada de vertice 0:"<<GRAPHindeg(G,0)<<endl;
+    GRAPHfree(G);
+    return 0;
+}
